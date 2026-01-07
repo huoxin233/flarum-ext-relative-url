f03f07d95e74cae08c4159b5a0b3fa86cbd90cd3
huoxin <23447157+huoxin233@users.noreply.github.com>
2026-01-07 08:23:32 +0000

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,7 +1,7 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('huoxin/relative-url', () => {
-  app.extensionData.for('huoxin-relative-url').registerSetting({
+  app.registry.for('huoxin-relative-url').registerSetting({
     setting: 'huoxin-relative-url.internal_domains',
     label: app.translator.trans('huoxin-relative-url.admin.internal_domains_label'),
     help: app.translator.trans('huoxin-relative-url.admin.internal_domains_help'),
