a11e1ed4d45fd3d1f8e17b42a4b3607dd6279609
huoxin <23447157+huoxin233@users.noreply.github.com>
2026-01-07 08:21:16 +0000

    chore(2.0): code formatting with prettier
    Start by formatting your code with Prettier.

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,13 +1,12 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('huoxin/relative-url', () => {
-  app.extensionData.for('huoxin-relative-url')
-    .registerSetting({
-      setting: 'huoxin-relative-url.internal_domains',
-      label: app.translator.trans('huoxin-relative-url.admin.internal_domains_label'),
-      help: app.translator.trans('huoxin-relative-url.admin.internal_domains_help'),
-      type: 'textarea',
-      rows: 8,
-      placeholder: app.translator.trans('huoxin-relative-url.admin.internal_domains_placeholder'),
-    });
+  app.extensionData.for('huoxin-relative-url').registerSetting({
+    setting: 'huoxin-relative-url.internal_domains',
+    label: app.translator.trans('huoxin-relative-url.admin.internal_domains_label'),
+    help: app.translator.trans('huoxin-relative-url.admin.internal_domains_help'),
+    type: 'textarea',
+    rows: 8,
+    placeholder: app.translator.trans('huoxin-relative-url.admin.internal_domains_placeholder'),
+  });
 });
