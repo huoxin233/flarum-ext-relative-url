57e3d258b33380bbe7f4e54735c70e9bd9516ec7
huoxin <23447157+huoxin233@users.noreply.github.com>
2026-01-08 09:03:38 +0000

    refactor: use admin extender

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,12 +1,1 @@
-import app from 'flarum/admin/app';
-
-app.initializers.add('huoxin/relative-url', () => {
-  app.registry.for('huoxin-relative-url').registerSetting({
-    setting: 'huoxin-relative-url.internal_domains',
-    label: app.translator.trans('huoxin-relative-url.admin.internal_domains_label'),
-    help: app.translator.trans('huoxin-relative-url.admin.internal_domains_help'),
-    type: 'textarea',
-    rows: 8,
-    placeholder: app.translator.trans('huoxin-relative-url.admin.internal_domains_placeholder'),
-  });
-});
+export { default as extend } from './extend';
